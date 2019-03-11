f4f3d33b351a7ca2c67fde4f65438ff7567675f4
Oleksandr Mochalin <a.y.mochalin@gmail.com>
2019-03-11 15:28:15 +0200

    RenameController

--- Controllers/CalcLendingCurValController.cs
+++ Controllers/CalcLendingCurValController.cs
@@ -11,25 +11,8 @@ namespace hackathon.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class CalcLendingCurValController : ControllerBase
-    {
-
-       /*
-        // GET api/CalcLendingCurVal
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-*/
-        // POST api/values
+    {      
+      // POST api/values
       //  [Route("")]
         [HttpPost]
         public CurValueResponse Post(CurValueRequest curValue)
